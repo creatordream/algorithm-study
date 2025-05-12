0667ad9ac4b61aad143dcaff3e5a391c20f7f75e
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-12 12:50:00 +0900

    Update 10093.cs

--- bojSolutions/2025-05-12/10093.cs
+++ bojSolutions/2025-05-12/10093.cs
@@ -1,1 +1,36 @@
+using System;
+using System.Text;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            string[] input = Console.ReadLine().Split();
+
+            long a = long.Parse(input[0]);
+            long b = long.Parse(input[1]);
+
+            long min = Math.Min(a, b);
+            long max = Math.Max(a, b);
+
+            long between = max - min - 1;
+
+            if (between <= 0) {
+                Console.WriteLine(0);
+                return;
+            }
+
+            Console.WriteLine(between);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (long i = min + 1; i < max; i++) {
+                sb.Append(i);
+                if (i < max - 1)
+                    sb.Append(" ");
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
