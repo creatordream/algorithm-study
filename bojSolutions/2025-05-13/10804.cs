5fb64c91b234346bb9b4c3b4040bf64c4cf01ae1
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-13 11:34:25 +0900

    Update 10804.cs

--- bojSolutions/2025-05-13/10804.cs
+++ bojSolutions/2025-05-13/10804.cs
@@ -1,1 +1,32 @@
+using System;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            int[] card = new int[20];
+            for(int i = 0; i < 20; i++)
+                card[i] = i + 1;
+            int temp;
+
+            for(int i = 0; i < 10; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                
+                int a = int.Parse(input[0]) - 1;
+                int b = int.Parse(input[1]) - 1;
+
+                while (a < b)
+                {
+                    temp = card[a];
+                    card[a] = card[b];
+                    card[b] = temp;
+                    a++;
+                    b--;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", card));
+        }
+    }
+}
