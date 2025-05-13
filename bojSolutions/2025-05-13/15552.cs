a8e2d6e5e9f39876d94e30d6f0c1e0898eae299c
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-13 12:08:38 +0900

    Update 15552.cs

--- bojSolutions/2025-05-13/15552.cs
+++ bojSolutions/2025-05-13/15552.cs
@@ -1,1 +1,26 @@
+using System;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            int t = int.Parse(Console.ReadLine());
+            int[] sum = new int[t];
+
+            for(int i = 0; i < t; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                
+                int a = int.Parse(input[0]);
+                int b = int.Parse(input[1]);
+
+                sum[i] = a + b;
+            }
+
+            for(int i = 0; i < t; i++)
+            {
+                Console.WriteLine(sum[i]);
+            }
+        }
+    }
+}
