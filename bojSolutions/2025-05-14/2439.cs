af1f54f5251e90eabd95a2272441a02843a75d9b
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-14 19:37:52 +0900

    Update 2439.cs

--- bojSolutions/2025-05-14/2439.cs
+++ bojSolutions/2025-05-14/2439.cs
@@ -1,1 +1,24 @@
+using System;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                for(int j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for(int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
