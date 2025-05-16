6b9645e90a56a9095a6b547cb784ff3cd1ba1046
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-16 11:15:07 +0900

    Update 2562.cs

--- bojSolutions/2025-05-16/2562.cs
+++ bojSolutions/2025-05-16/2562.cs
@@ -1,1 +1,25 @@
+using System;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            int max = 0; //최댓값
+            int count = 0; //최댓값의 위치
+            int[] num = new int[9];
+
+            for(int i = 0; i < 9; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+                if(max < num[i])
+                {
+                    max = num[i];
+                    count = i + 1;
+                }
+            }
+
+            Console.WriteLine(max);
+            Console.WriteLine(count);
+        }
+    }
+}
