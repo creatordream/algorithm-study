6f646fdcfebd88d0aef237fb6dfad3426f040f5a
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-16 12:37:53 +0900

    Update 10171.cs

--- bojSolutions/2025-05-09/10171.cs
+++ bojSolutions/2025-05-09/10171.cs
@@ -1,1 +1,14 @@
+using System;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+
+        }
+    }
+}
