7bd85c82959e9304871e22f8163c3c83134b543d
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-16 10:53:37 +0900

    Update 2446.cs

--- bojSolutions/2025-05-16/2446.cs
+++ bojSolutions/2025-05-16/2446.cs
@@ -1,1 +1,45 @@
+using System;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= 2 * n - 1; i++)
+            {
+                int level; //층 수
+                int space; //공백
+                int stars; //별
+                if(i <= n)
+                {
+                    level = i;
+                }
+                else
+                {
+                    level = 2 * n - i;
+                }
+
+                space = level - 1;
+                stars = 2 * (n - level) + 1;
+
+                for(int j = 0; j < space; j++)
+                {
+                    Console.Write(" ");
+                }
+                
+                for(int k = 0; k < stars; k++)
+                {
+                    Console.Write("*");
+                }
+
+                for(int l = 0; l < space; l++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
