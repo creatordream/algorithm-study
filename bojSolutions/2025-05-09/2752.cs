98aff731138f012b75b3046bb9637c9225de1fdc
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-16 12:39:05 +0900

    Update 2752.cs

--- bojSolutions/2025-05-09/2752.cs
+++ bojSolutions/2025-05-09/2752.cs
@@ -1,1 +1,26 @@
+using System;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            string[] input = Console.ReadLine().Split();
+
+            int[] numbers = Array.ConvertAll(input, int.Parse);
+
+            for(int i = 0; i < numbers.Length - 1; i++)
+            {
+                for(int j = 0; j < numbers.Length -i - 1; j++)
+                {
+                    if(numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
