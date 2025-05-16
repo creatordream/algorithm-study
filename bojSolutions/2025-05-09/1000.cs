42b157dcaae17ec36ff7770403f6320285443e62
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-16 12:37:36 +0900

    Update 1000.cs

--- bojSolutions/2025-05-09/1000.cs
+++ bojSolutions/2025-05-09/1000.cs
@@ -1,1 +1,19 @@
+using System;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(" ");
+
+            int A = int.Parse(numbers[0]);
+            int B = int.Parse(numbers[1]);
+
+            int sum = A + B;
+
+            Console.WriteLine("{0}", sum);
+
+        }
+    }
+}
