b92d2b85908db7ba6e14fdd853b91d61dad3b39c
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-16 12:38:45 +0900

    Update 9498.cs

--- bojSolutions/2025-05-09/9498.cs
+++ bojSolutions/2025-05-09/9498.cs
@@ -1,1 +1,33 @@
+using System;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            string input = Console.ReadLine();
+
+            int score = int.Parse(input);
+
+            if(score <= 100 && score >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if(score < 90 && score >= 80)
+            {
+                Console.WriteLine("B");
+            }
+            else if(score < 80 && score >= 70)
+            {
+                Console.WriteLine("C");
+            }
+            else if(score < 70 && score >= 60)
+            {
+                Console.WriteLine("D");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+        }
+    }
+}
