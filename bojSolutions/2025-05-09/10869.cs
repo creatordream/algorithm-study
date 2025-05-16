799e6b39b810021553988ea1f9adeba34f0ca18c
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-16 12:38:28 +0900

    Update 10869.cs

--- bojSolutions/2025-05-09/10869.cs
+++ bojSolutions/2025-05-09/10869.cs
@@ -1,1 +1,21 @@
 
+using System;
+
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(" ");
+
+            int A = int.Parse(numbers[0]);
+            int B = int.Parse(numbers[1]);
+
+            Console.WriteLine("{0}", A + B);
+            Console.WriteLine("{0}", A - B);
+            Console.WriteLine("{0}", A * B);
+            Console.WriteLine("{0}", A / B);
+            Console.WriteLine("{0}", A % B);
+        }
+    }
+}
