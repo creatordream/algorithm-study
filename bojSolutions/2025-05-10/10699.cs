e41d86e6061143c2bba4238a524ee40ab0a67d6f
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-16 12:39:55 +0900

    Update 10699.cs

--- bojSolutions/2025-05-10/10699.cs
+++ bojSolutions/2025-05-10/10699.cs
@@ -1,1 +1,11 @@
+using System;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            DateTime seoulTime = DateTime.Now + TimeSpan.FromHours(9);
+            Console.WriteLine(seoulTime.ToString("yyyy-MM-dd"));
+        }
+    }
+}
