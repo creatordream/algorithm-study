ff45a5a67f9b63ae00f311189e089142b2274317
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-19 10:27:53 +0900

    Update 10808.cs

--- bojSolutions/2025-05-18/10808.cs
+++ bojSolutions/2025-05-18/10808.cs
@@ -5,16 +5,16 @@ namespace 백준
     class HelloWorld {
         static void Main(string[] args) {
             string input = Console.ReadLine();
-			int[] counts = new int[26];
+	    int[] counts = new int[26];
 			
-			for (int i = 0; i < input.Length; i++)
-			{
-			    char c = input[i];
-			    if (c >= 'a' && c <= 'z')
-			    {
-			        counts[c - 'a']++;
-			    }
-			}
+	    for (int i = 0; i < input.Length; i++)
+	    {
+	        char c = input[i];
+		if (c >= 'a' && c <= 'z')
+		{
+		    counts[c - 'a']++;
+		}
+	    }
 
             for (int i = 0; i < 26; i++) {
                 Console.Write(counts[i]);
