4180ba034ae46240edffe636bd8d9fb9e7b4e511
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-19 13:33:02 +0900

    Update 1475.cs

--- bojSolutions/2025-05-19/1475.cs
+++ bojSolutions/2025-05-19/1475.cs
@@ -6,17 +6,20 @@ namespace 백준
         static void Main(string[] args) {
             string roomNum = Console.ReadLine();
             int[] cnt = new int[10];
+
             foreach (char c in roomNum)
                 cnt[c - '0']++;
-            int setCount = 0;
+
+            // 6/9는 뒤집어 쓸 수 있으므로 합쳐서 올림 나누기
             int total = cnt[6] + cnt[9];
-            int need = (total + 1) / 2;
-            cnt[6] = need;
+            cnt[6] = (total + 1) / 2;
             cnt[9] = 0;
 
-
+            // 가장 많이 필요한 숫자 개수가 세트 수
+            int setCount = 0;
             for (int d = 0; d < 10; d++)
                 setCount = Math.Max(setCount, cnt[d]);
+
             Console.WriteLine(setCount);
         }
     }
