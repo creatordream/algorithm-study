a3926d0b65725643bc35496d5593e3a6514f9b61
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-19 13:32:15 +0900

    Update 1475.cs

--- bojSolutions/2025-05-19/1475.cs
+++ bojSolutions/2025-05-19/1475.cs
@@ -1,1 +1,23 @@
+using System;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            string roomNum = Console.ReadLine();
+            int[] cnt = new int[10];
+            foreach (char c in roomNum)
+                cnt[c - '0']++;
+            int setCount = 0;
+            int total = cnt[6] + cnt[9];
+            int need = (total + 1) / 2;
+            cnt[6] = need;
+            cnt[9] = 0;
+
+
+            for (int d = 0; d < 10; d++)
+                setCount = Math.Max(setCount, cnt[d]);
+            Console.WriteLine(setCount);
+        }
+    }
+}
