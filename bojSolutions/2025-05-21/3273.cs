a8c83cf67b5684bf33086109d50094fe0a0dd328
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-21 10:52:25 +0900

    Update 3273.cs

--- bojSolutions/2025-05-21/3273.cs
+++ bojSolutions/2025-05-21/3273.cs
@@ -1,1 +1,37 @@
+using System;
+using System.Linq;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int x = int.Parse(Console.ReadLine());
+            int left = 0, right = n - 1, count = 0;
+
+            Array.Sort(a);
+
+            while(left < right)
+            {
+                int sum = a[left] + a[right];
+                if(sum == x)
+                {
+                    count++;
+                    left++;
+                    right--;
+                }
+                else if(sum < x)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
