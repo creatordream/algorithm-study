fa902a1b6fc692e9af59107fc5bb50b4b82edf7b
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-21 11:00:39 +0900

    Update 10807.cs

--- bojSolutions/2025-05-21/10807.cs
+++ bojSolutions/2025-05-21/10807.cs
@@ -1,1 +1,23 @@
+using System;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int v = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for(int i = 0; i < a.Length; i++)
+            {
+                if(a[i] == v)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
