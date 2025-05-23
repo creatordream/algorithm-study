d904a519ff1010d958b5db794c979379e42ec899
creatordream <129447265+creatordream@users.noreply.github.com>
2025-05-23 10:46:16 +0900

    Update 1919.cs

--- bojSolutions/2025-05-23/1919.cs
+++ bojSolutions/2025-05-23/1919.cs
@@ -1,1 +1,24 @@
+using System;
 
+namespace 백준
+{    
+    class HelloWorld {
+        static void Main(string[] args) {
+            int deleteWord = 0;
+            int[] cnt1 = new int[26];
+            int[] cnt2 = new int[26];
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+
+            foreach (char c in input1) cnt1[c - 'a']++;
+            foreach (char c in input2) cnt2[c - 'a']++;
+
+            for(int i = 0; i < 26; i++)
+            {
+                deleteWord += Math.Abs(cnt1[i] - cnt2[i]);
+            }
+
+            Console.Write(deleteWord);
+        }
+    }
+}
